949bc2716cb3a38ee4ac2ee310fe6ffa86e6c4ba
Adrian Castillo <adrian.cast@icloud.com>
2026-03-25 20:13:57 +0100

    (particle life) Initialize the particles inside 'particles'

--- unity/CpuParticleSimulations/Assets/SimulationEngine/Simulations/ParticleLifeCPU.cs
+++ unity/CpuParticleSimulations/Assets/SimulationEngine/Simulations/ParticleLifeCPU.cs
@@ -48,7 +48,11 @@ namespace SimulationEngine.Simulations
         return rows;
     }
 
-    private Particle[] InitializeParticles() {
+    private Particle[] InitializeParticles()
+    {
+        GameObject particlesParent = new GameObject();
+        particlesParent.name = "particles";
+        
         Particle[] particles = new Particle[n];
         for (int i = 0; i < n; i++) {
             particles[i].position = Random.insideUnitSphere * simSize;
@@ -56,6 +60,7 @@ namespace SimulationEngine.Simulations
             particles[i].type = Random.Range(0, types);
             particles[i].gameObject = Instantiate(particlePrefab, particles[i].position, Quaternion.identity);
             particles[i].gameObject.transform.localScale = Vector3.one * particleRadius;
+            particles[i].gameObject.transform.parent = particlesParent.transform; // Initializes the particles inside 'particlesParent'
             
             float hue = (float)particles[i].type / (float)types;
             particles[i].gameObject.GetComponent<Renderer>().material.color = Color.HSVToRGB(hue, 1, 1);
