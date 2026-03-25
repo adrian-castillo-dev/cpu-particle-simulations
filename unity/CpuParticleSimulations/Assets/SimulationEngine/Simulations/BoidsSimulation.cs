ba14d854117431d94e3e9b5de4e694b63e7dc9c4
Adrian Castillo <adrian.cast@icloud.com>
2026-03-25 22:22:24 +0100

    (boids) Implemented Alignment and Cohesion

--- unity/CpuParticleSimulations/Assets/SimulationEngine/Simulations/BoidsSimulation.cs
+++ unity/CpuParticleSimulations/Assets/SimulationEngine/Simulations/BoidsSimulation.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using SimulationEngine.Core;
+using Unity.Mathematics;
 
 namespace SimulationEngine.Simulations
 {
@@ -13,6 +14,13 @@ namespace SimulationEngine.Simulations
         [SerializeField] private float protectedRange;
         [SerializeField] private float visualRange;
         [SerializeField] private float avoidFactor;
+        [SerializeField] private float matchingFactor;
+        [SerializeField] private float centeringFactor;
+
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float minSpeed;
+
+        [SerializeField] private float turnFactor;
         
         private Particle[] particles;
         private ParticleManager particleManager;
@@ -30,8 +38,11 @@ namespace SimulationEngine.Simulations
                 Particle boid = particles[i];
                 
                 Vector3 close = Vector3.zero;
+                Vector3 averageVelocity = Vector3.zero;
+                Vector3 averagePosition = Vector3.zero;
+                float neighboringBoids = 0;
 
-                // Loop thru every other boid
+                // Loop through every other boid
                 for (int j = 0; j < boidsCount; j++)
                 {
                     if (i == j) continue;
@@ -39,18 +50,71 @@ namespace SimulationEngine.Simulations
                     Particle otherBoid = particles[j];
 
                     float distance = Vector3.Distance(boid.position, otherBoid.position);
+                    
                     if (distance < protectedRange)
                     {
                         close += boid.position - otherBoid.position;
                     }
+
+                    if (distance < visualRange)
+                    {
+                        averageVelocity += otherBoid.velocity;
+                        averagePosition += otherBoid.position;
+                        neighboringBoids += 1;
+                    }
+                }
+
+                if (neighboringBoids > 0)
+                {
+                    averageVelocity /= neighboringBoids;
+                    averagePosition /= neighboringBoids;
                 }
 
                 boid.velocity += close * avoidFactor;
+                boid.velocity += (averageVelocity - boid.velocity) * matchingFactor;
+                boid.velocity += (averagePosition - boid.position) * centeringFactor;
+
+                float margin = simulationSize;
+
+                if (boid.position.x < -margin) {
+                    boid.velocity.x += turnFactor;
+                }
+                if (boid.position.x > margin) {
+                    boid.velocity.x -= turnFactor;
+                }
+                
+                if (boid.position.y < -margin) {
+                    boid.velocity.y += turnFactor;
+                }
+                if (boid.position.y > margin) {
+                    boid.velocity.y -= turnFactor;
+                }
+                
+                if (boid.position.z < -margin) {
+                    boid.velocity.z += turnFactor;
+                }
+                if (boid.position.z > margin) {
+                    boid.velocity.z -= turnFactor;
+                }
+                
+
+                float speed = math.sqrt(boid.velocity.x*boid.velocity.x + boid.velocity.y*boid.velocity.y + boid.velocity.z*boid.velocity.z);
+
+                if (speed > maxSpeed)
+                {
+                    boid.velocity = (boid.velocity / speed) * maxSpeed;
+                }
+
+                if (speed < minSpeed)
+                {
+                    boid.velocity = (boid.velocity / speed) * minSpeed;
+                }
                 
                 particles[i] = boid;
-                UpdateParticleVisuals(particles);
             }
             
+            UpdateParticleVisuals(particles);
+
         }
         
         private void UpdateParticleVisuals(Particle[] particles) {
